de502387ef1cbe9a0dbec0320a49a2ae6110ae56
ElMati217 <101896135+ElMati217@users.noreply.github.com>
2022-04-08 19:31:01 +0200

    Add files via upload

---
+++ HolaExamen.java
@@ -0,0 +1,8 @@
+
+public class HolaExamen {
+	public static void main(String[] args) {
+			
+			System.out.println("Soc Iker i estic fent l’examen de la UF2 del M01 de DAM. Tortura eterna para los que decidan traicionar la intención de Cristo");
+	
+		}
+}
