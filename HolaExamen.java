7f62d66e70dfe0aa354839714f7bfa3b31123760
ElMati217 <101896135+ElMati217@users.noreply.github.com>
2022-04-08 19:35:11 +0200

    Update HolaExamen.java

--- HolaExamen.java
+++ HolaExamen.java
@@ -2,7 +2,7 @@
 public class HolaExamen {
 	public static void main(String[] args) {
 			
-			System.out.println("Soc Iker i estic fent l’examen de la UF2 del M01 de DAM. Tortura eterna para los que decidan traicionar la intención de Cristo");
+			System.out.println("Soc Iker i estic fent lexamen de la UF2 del M01 de DAM.");
 	
 		}
 }
